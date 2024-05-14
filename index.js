fbcfd06a6ab2f39e1c1681d84f2dc3233607b91d
Joaquin <joaquinyablonovich@hotmail.com>
2024-05-14 00:34:14 -0300

    Menu responsive

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+
+
 document.addEventListener('DOMContentLoaded',function(){
+
+
+    let IconoMenu = document.getElementById('Icono');
+    let Menu = document.querySelector('.Menu'); // Selecciona el primer elemento con la clase 'Menu'
+
+    IconoMenu.addEventListener('click', function() {
+        if (Menu.style.display === 'flex') {
+            Menu.style.display = 'none';
+        } else {
+            Menu.style.display = 'flex';
+            Menu.style.flexDirection= 'row'
+            
+        }
+        });
+
+
+
     const searchInput = document.getElementById('Filtrar')
     const list = document.getElementById('lista').getElementsByTagName('li')
 
@@ -16,4 +35,5 @@ document.addEventListener('DOMContentLoaded',function(){
             }
         }
     })
+
 })
